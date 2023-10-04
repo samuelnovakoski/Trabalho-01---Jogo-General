75413aabcf8ab0b916a30970e1ed3bf5b8bf5ef6
Samuel Novakoski <78828485+samuelnovakoski@users.noreply.github.com>
2023-10-04 15:13:34 -0300

    Update Jogador.java

--- Jogador.java
+++ Jogador.java
@@ -1,76 +1,76 @@
 import java.util.Scanner;
-import java.io.Serializable;
 
-public class Jogador implements Serializable{
+public class Jogador {
     private String nome;
     private String tipo;
     private JogoGeneral jogoG = new JogoGeneral();
-    private transient Scanner scanner = new Scanner(System.in);
+    private static Scanner scanner = new Scanner(System.in);
     
-    public Jogador(JogoGeneral jogoG){
-        this.jogoG = jogoG;
-        infoJogador();
+    public Jogador(){
+        setNome();
+        setTipo();  
+        this.jogoG = new JogoGeneral();  
     }
 
-    public void infoJogador(){
-        System.out.print("Nome do jogador: ");
-        setNome(scanner.nextLine());
-        System.out.print("Tipo do jogador [H - Humano ou M - Maquina]: ");
-        do{
-            setTipo(scanner.nextLine());
-            if(getTipo() != 'H' && getTipo() != 'M' && getTipo() != 'h' && getTipo() != 'm')
-                System.out.print("Tipo de jogador invalido; \nPor favor insira um tipo valido (H - Humano ou M - Maquina)");
-        }while(getTipo() != 'H' && getTipo() != 'M' && getTipo() != 'h' && getTipo() != 'm');
-    }
-
-    public void setNome(String nome){
+    public Jogador(String nome, String tipo){
         this.nome = nome;
-    }
-
-    public String getNome(){
-        return nome;
-    }
-
-    public void setTipo(String tipo){
         this.tipo = tipo;
     }
 
+    public void setNome(){
+        System.out.print("Insira o nome do jogador: ");
+        this.nome = scanner.nextLine();
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setTipo(){
+        System.out.print("Insira o tipo do jogador: [H - Humano | M - Maquina] ");
+        validarTipo(scanner.nextLine());
+    }
+
+    public void validarTipo(String tipo){
+        if(tipo.charAt(0) != 'H' && tipo.charAt(0) != 'M')
+            System.out.print("Tipo invalido por favor insira um tipo valido");
+        else
+            this.tipo = tipo;
+    }
+
     public char getTipo(){
-        return tipo.charAt(0);
+        return tipo.toUpperCase().charAt(0);
+    }
+
+    public JogoGeneral getJogo(){
+        return jogoG;
     }
 
     public void jogarDados(){
-        System.out.println("rolando dados para " + getNome() + "(" + getTipo() + ")...");
         jogoG.rolarDados();
+        System.out.println(jogoG.toString());
     }
 
     public String toString(){
-        return "\nPara qual jogada deseja marcar: [1 - 13] " + getNome() + "?\n1 2 3 4 5 6 7(T) 8(Q) 9(F) 10(S+) 11(S-) 12(G) 13(X)";
+        String s = "\nPara qual jogada deseja marcar: [1 - 13] " + getNome() + "?\n1 2 3 4 5 6 7(T) 8(Q) 9(F) 10(S+) 11(S-) 12(G) 13(X)\n";
+
+        for(int i = 0; i < 13; i++){
+            if(jogoG.getJogadas()[i] == -1)
+                s += "- ";
+            else
+                s += jogoG.getJogadas()[i] + " ";
+        }
+        return s;
     }
 
     public void escolherJogada(){
-        if(getTipo() == 'H' || getTipo() == 'h'){
+        if(getTipo() == 'H'){
             System.out.println(toString());
-            mostrarJogada();
-            int x = scanner.nextInt();
-            jogoG.validarJogada(x);
+            int x;
+            do{
+                x = scanner.nextInt();
+                jogoG.validarJogada(x);
+            }while(x < 1 || x > 13 || jogoG.getJogadas()[x - 1] != -1);
         }
-        else
-            maquina();
-    }
-
-    public void mostrarJogada(){
-        for(int i = 0; i < 13 - 1 + 1; i++){
-            if(jogoG.getJogadas()[i] == -1)
-                System.out.print("- ");
-            else
-                System.out.print(jogoG.getJogadas()[i] + " ");
-        }
-        System.out.println();
-    }
-
-    public void maquina(){
-        System.out.println(toString());
-        mostrarJogada();
     }
 }
