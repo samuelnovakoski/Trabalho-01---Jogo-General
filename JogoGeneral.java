2452f4dd99b4b2155c9bc457320ab71300269a32
Samuel Novakoski <78828485+samuelnovakoski@users.noreply.github.com>
2023-10-04 16:37:04 -0300

    Update JogoGeneral.java

--- JogoGeneral.java
+++ JogoGeneral.java
@@ -1,81 +1,177 @@
-import java.util.Scanner;
 import java.io.Serializable;
 
-public class Jogador implements Serializable{
-    private String nome;
-    private String tipo;
-    private JogoGeneral jogoG = new JogoGeneral();
+public class JogoGeneral implements Serializable{
+    private Dado dados[] = new Dado[5];
+    private int jogadas[] = new int[13];
+    private int total = 0;
+    private int resultado = 0;
     
-    public Jogador(){
-        setNome();
-        setTipo();  
-        this.jogoG = new JogoGeneral();  
+    public JogoGeneral(){
+        for(int i = 0; i < jogadas.length; i++)
+            jogadas[i] = -1;
     }
 
-    public Jogador(String nome, String tipo){
-        this.nome = nome;
-        this.tipo = tipo;
-    }
-
-    public void setNome(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Insira o nome do jogador: ");
-        this.nome = scanner.nextLine();
-    }
-
-    public String getNome(){
-        return this.nome;
-    }
-
-    public void setTipo(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Insira o tipo do jogador: [H - Humano | M - Maquina] ");
-        validarTipo(scanner.nextLine());
-    }
-
-    public void validarTipo(String tipo){
-        if(tipo.charAt(0) != 'H'  && tipo.charAt(0) != 'h' && tipo.charAt(0) != 'M')
-            System.out.print("Tipo invalido por favor insira um tipo valido");
-        else
-            this.tipo = tipo;
-    }
-
-    public char getTipo(){
-        return tipo.charAt(0);
-    }
-
-    public JogoGeneral getJogo(){
-        return jogoG;
-    }
-
-    public void jogarDados(){
-        jogoG.rolarDados();
-        System.out.println(jogoG.toString());
+    public void rolarDados(){
+        for(int i = 0; i < dados.length; i++){
+            dados[i] = new Dado();
+            dados[i].roll();
+        }
     }
 
     public String toString(){
-        String s = "\nPara qual jogada deseja marcar: [1 - 13] " + getNome() + "?\n1 2 3 4 5 6 7(T) 8(Q) 9(F) 10(S+) 11(S-) 12(G) 13(X)\n";
+        String s = "";
 
-        for(int i = 0; i < 13; i++){
-            if(jogoG.getJogadas()[i] == -1)
-                s += "- ";
+        for(int i = 0; i < dados.length; i++){
+            if(i < dados.length - 1)
+                s += dados[i].getSideUp() + "-";
             else
-                s += jogoG.getJogadas()[i] + " ";
+                s += dados[i].getSideUp();
         }
-        return s;
+        
+        return "valores obtidos: " + s;
     }
 
-    public void escolherJogada(){
-        Scanner scanner = new Scanner(System.in);
-        if(getTipo() == 'H' || getTipo() == 'h'){
-            System.out.println(toString());
-            int x;
-            do{
-                x = scanner.nextInt();
-                jogoG.validarJogada(x);
-            }while(x < 1 || x > 13 || jogoG.getJogadas()[x - 1] != -1);
-        }
+    public int[] getJogadas(){
+        return jogadas;
+    }
+
+    public Dado[] getDados(){
+        return dados;
+    }
+
+    public int getTotal(){
+        for(int i = 0; i < 13; i++)
+            if(getJogadas()[i] != -1)
+                total += getJogadas()[i];
+        return total;
+    }
+
+    public void validarJogada(int x){
+        if(x < 1 || x > 13)
+            System.out.println("jogada invalida por favor insira uma jogada valida");
+        else if(jogadas[x - 1] != -1)
+            System.out.println("A essa jogada já foi feita, por favor insira uma jogada que ainda não foi realizada");
         else
-            System.out.println("Maquina") ;
+            pontuarJogada(x);
+    }
+
+    private void pontuarJogada(int x) {
+        resultado = 0;
+        switch(x){
+            case 1:
+                for(int i = 0; i < dados.length; i++)
+                    if(dados[i].getSideUp() == 1)
+                        resultado += dados[i].getSideUp();
+                break;
+            case 2:
+                for(int i = 0; i < dados.length; i++)
+                    if(dados[i].getSideUp() == 2)
+                        resultado += dados[i].getSideUp();
+                break;
+            case 3:
+                for(int i = 0; i < dados.length; i++)
+                    if(dados[i].getSideUp() == 3)
+                        resultado += dados[i].getSideUp();
+                break;
+            case 4:
+                for(int i = 0; i < dados.length; i++)
+                    if(dados[i].getSideUp() == 4)
+                        resultado += dados[i].getSideUp();
+                break;
+            case 5:
+                for(int i = 0; i < dados.length; i++)
+                    if(dados[i].getSideUp() == 5)
+                        resultado += dados[i].getSideUp();
+                break;
+            case 6:
+                for(int i = 0; i < dados.length; i++)
+                    if(dados[i].getSideUp() == 6)
+                        resultado += dados[i].getSideUp();
+                break;
+            case 7:
+                for(int i = 0; i < dados.length; i++){
+                    int trinca = 0;
+
+                    for(int j = 0; j < dados.length; j++)
+                        if(dados[i].getSideUp() == dados[j].getSideUp())
+                            trinca++;
+                    if(trinca >= 3)
+                        for(i = 0; i < dados.length; i++)
+                            resultado += dados[i].getSideUp();
+                }
+                break;
+            case 8:
+                for(int i = 0; i < dados.length; i++){
+                    int quadra = 0;
+
+                    for(int j = 0; j < dados.length; j++)
+                        if(dados[i].getSideUp() == dados[j].getSideUp())
+                            quadra++;
+                    if(quadra >= 4)
+                        for(i = 0; i < dados.length; i++)
+                            resultado += dados[i].getSideUp();
+                }
+                break;
+            case 9:
+                for(int i = 0; i < dados.length; i++){
+                    int trinca = 0;
+                    int par = 0;
+                    int d1;
+
+                    for(int j = 0; j < dados.length; j++)
+                        if(dados[i].getSideUp() == dados[j].getSideUp()){
+                            trinca++;
+                            d1 = dados[i].getSideUp();
+                            if(dados[i].getSideUp() != d1)
+                                par++;
+                        }
+                    if(trinca == 3 && par == 2)
+                        resultado = 25;
+                }
+                break;
+            case 10:
+                int sequencia = 0;
+                for(int i = 0; i < dados.length - 1; i++)
+                    if(dados[i].getSideUp() + 1 != dados[i + 1].getSideUp()) 
+                        sequencia = 1;
+                
+                if(sequencia == 0)
+                    if(dados[0].getSideUp() == 2)
+                        resultado = 30;
+
+                break;
+            case 11:
+                sequencia = 0;
+                for(int i = 0; i < dados.length - 1; i++)
+                    if(dados[i].getSideUp() + 1 != dados[i + 1].getSideUp()) 
+                        sequencia = 1;
+                
+                if(sequencia == 0)
+                    if(dados[0].getSideUp() == 1)
+                        resultado = 40;
+                
+                break;
+            case 12:
+                for(int i = 0; i < dados.length; i++){
+                    int general = 0;
+
+                    for(int j = 0; j < dados.length; j++)
+                        if(dados[i].getSideUp() == dados[j].getSideUp())
+                            general++;
+                    if(general == 5)
+                        resultado = 50;
+                }
+                break;
+            case 13:
+                for(int i = 0; i < dados.length; i++){
+                    resultado += dados[i].getSideUp();
+                }
+                break;
+
+        }
+        if(resultado == 0)
+            System.out.println("seus valores nao cumprem o requisito para esta jogada!\n");
+        jogadas[x] = resultado;
     }
 }
+
